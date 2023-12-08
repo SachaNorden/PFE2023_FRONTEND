0b47c9480314786e8ffc5b50fbeda4a54a092cf5
maxime.vanrechem@student.vinci.be <Ourson=2000>
2023-12-08 13:47:08 +0100

    close branch

--- app/(itineraires)/(routes)/itineraires/page.tsx
+++ app/(itineraires)/(routes)/itineraires/page.tsx
@@ -5,7 +5,9 @@ import { Button } from '@/components/ui/button'; // Example path, adjust accordi
 export default function Itineraires() {
     return (
         <div className='min-h-screen flex flex-col '>
-            <p className="text-6xl flex flex-col justify-center items-center">itineraires</p>
+            <Image width={65} src="/Snappies-Logo.png" preview={false} className=' ' />
+
+            <p className="text-4xl flex flex-col justify-center items-center">Feuilles de routes</p>
             <Form
                 initialValues={{remember: true}}
                 autoComplete="off"
@@ -18,19 +20,33 @@ export default function Itineraires() {
                   relative
                   z-20'
             >
-                <p>- <b>itinéraire 1</b> : livreur1.nom <p className="font-size:4px">DEPOT</p></p>
+                <p className="text-base">- <b className="text-lg">itinéraire 1</b> : livreur1.nom <span
+                    className="text-xl text-red-300 font-bold">DEPOT</span></p>
+                <p className="text-sm"> creche1 - creche2 - creche3 - creche4</p>
                 <p>---------------------------------</p>
-                <p>- <b>itinéraire 2</b> : livreur2.nom</p>
+                <p className="text-base">- <b className="text-lg">itinéraire 2</b> : livreur2.nom <span
+                    className="text-xl text-green-300 font-bold">EN COURS</span></p>
+                <p className="text-sm"> creche1 - creche2 - creche3 - creche4</p>
                 <p>---------------------------------</p>
-                <p>- <b>itinéraire 3</b> : livreur3.nom</p>
+                <p className="text-base">- <b className="text-lg">itinéraire 3</b> : livreur3.nom <span
+                    className="text-xl text-green-300 font-bold">EN COURS</span></p>
+                <p className="text-sm"> creche1 - creche2 - creche3 - creche4</p>
                 <p>---------------------------------</p>
-                <p>- <b>itinéraire 4</b> : livreur4.nom</p>
+                <p className="text-base text-gray-400">- <b className="text-lg">itinéraire 4</b> : livreur4.nom <span
+                    className="text-xl font-bold">FINIE</span></p>
+                <p className="text-sm text-gray-400"> creche1 - creche2 - creche3 - creche4</p>
                 <p>---------------------------------</p>
-                <p>- <b>itinéraire 5</b> : livreur5.nom</p>
+                <p className="text-base text-gray-400">- <b className="text-lg">itinéraire 5</b> : livreur5.nom <span
+                    className="text-xl font-bold">FINIE</span></p>
+                <p className="text-sm text-gray-400"> creche1 - creche2 - creche3 - creche4</p>
                 <p>---------------------------------</p>
-                <p>- <b>itinéraire 6</b> : livreur6.nom</p>
+                <p className="text-base text-gray-400">- <b className="text-lg">itinéraire 6</b> : livreur6.nom <span
+                    className="text-xl font-bold">FINIE</span></p>
+                <p className="text-sm text-gray-400"> creche1 - creche2 - creche3 - creche4</p>
                 <p>---------------------------------</p>
-                <p>+</p>
+                {/* eslint-disable-next-line jsx-a11y/alt-text */}
+                <Image width={60} src="/plus.png" preview={false} className='absolute -top-21 left-1/2 transform -translate-x-1/2 -z10 -translate-y-5' />
+
             </Form>
         </div>
     )
