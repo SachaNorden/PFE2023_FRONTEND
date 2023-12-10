00d833d3f19aefb41a9f4b10b04d6b53a72325cd
maxime.vanrechem@student.vinci.be <Ourson=2000>
2023-12-10 16:32:52 +0100

    itineraires vue livreur

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
 
-    publicRoutes: ["/login", "/testArticles", "/itineraires", "/itineraires/route", "/itineraires/details", "/users/:id"]
+    publicRoutes: ["/login", "/testArticles", "/itineraires", "/itineraires/route",  "/itineraires/livreur", "/itineraires/details", "/users/:id"]
 
 });
 
