5400c0a259a0b11f323ac18da41c28bfb2825c16
maxime.vanrechem@student.vinci.be <Ourson=2000>
2023-12-14 11:01:16 +0100

    Menu Deroulant update

--- app/ui/menu/menu.tsx
+++ app/ui/menu/menu.tsx
@@ -25,14 +25,11 @@ function MenuDer() {
                         <Menu.Item key="newClient">
                             <a href="/clients/ajouterClient">Nouveau Client</a>
                         </Menu.Item>
-                        <Menu.Item key="newCommande">
-                            <a href="/commandes/creation">Nouvelle Commande</a>
-                        </Menu.Item>
                         <Menu.Item key="listeClients">
                             <a href="/clients">Liste des Clients</a>
                         </Menu.Item>
                         <Menu.Item key="listeLivreurs">
-                            <a href="/users">Liste des Clients</a>
+                            <a href="/users">Liste des Livreurs</a>
                         </Menu.Item>
                     </>
                 ) : (<></>)}
